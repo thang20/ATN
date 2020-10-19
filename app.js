82932e91a548e928b154d636ff8589e1d3137a36
unknown <thangnngch18753@fpt.edu.vn>
2020-10-19 16:02:30 +0700

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const engines = require('consolidate');
 const app = express();
-
+var exphbs  = require('express-handlebars');
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -10,11 +10,13 @@ var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
 //npm i handlebars consolidate --save
-app.engine('hbs',engines.handlebars);
-app.set('views','./views');
-app.set('view engine','hbs');
+app.set('views', __dirname + '/views');
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: false}))
+app.set('view engine', '.hbs');
 
-var MongoClient = require('mongodb').MongoClient;
+//app.set('view engine', 'ejs');
+
+var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb+srv://ngocthang:ngocthang@cluster0.rwjhi.mongodb.net/test';
 
 app.get('/all',async function(req,res){
@@ -25,7 +27,9 @@ app.get('/all',async function(req,res){
 })
 
 app.get('/add', function(req,res){
-    res.render('addSanPham');
+   console.log("dsadss");
+   res.render('addSanPham');
+   
 });
 
 app.post('/doAddProduct',async function(req,res){
@@ -39,9 +43,9 @@ app.post('/doAddProduct',async function(req,res){
     let client= await MongoClient.connect(url);
     let dbo = client.db("asm2ATN");
     await dbo.collection("product").insertOne(insertProducts, (err, results)=>{
-        console.log(results)
-        if(err) return console.log(err)
-        console.log('saved to data')
+    console.log(results)
+    if(err) return console.log(err)
+    console.log('saved to data')
 
     });
     let results = await dbo.collection("product").find({}).toArray();
@@ -62,15 +66,42 @@ app.get('/delete',async function(req,res){
 })
 
 app.get('/edit',async function(req,res){
-    // let id = req.query.id;
-    // var ObjectID = require('mongodb').ObjectID;
-    // let condition = {"_id" : ObjectID(id)};
+    let id = req.query.id;
+    var ObjectID =  require('mongodb').ObjectID;
 
-    // let client= await MongoClient.connect(url);
-    // let dbo = client.db("asm2ATN");
-    // await dbo.collection("product").deleteOne(condition);
-    // res.redirect('/all');
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("asm2ATN");
+    let results = await dbo.collection("product").findOne({"_id" : ObjectID(id)});
+    res.render('editSanPham',{sanPham:results});
+  
 })
+
+app.post('/doEditProduct', async function(req,res){
+    let id = req.body._id;
+    let name = req.body.name;
+    let price = req.body.price;
+    let category = req.body.category;
+  
+
+    let newValue = { $set: {name : name, price : price, category : category,}}
+    var ObjectID = require('mongodb').ObjectID;
+    let condition = {"_id" : ObjectID(id)};
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("asm2ATN");
+    await dbo.collection("product").updateOne(condition, insertProducts, (err, results)=>{
+    console.log(results)
+    if(err) return console.log(err)
+    console.log('saved to data')
+
+    }) ;
+    let results = await dbo.collection("product").find({}).toArray();
+    res.render('allSanPham',{sanPham:results});
+})
+
+
+
+
+
 const PORT = process.env.PORT || 5000
 var server=app.listen(PORT,function() {
     console.log("Server is running at " + PORT);
